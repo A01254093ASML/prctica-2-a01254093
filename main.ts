9179d91abc5c713773bdaf94c34b3c9f6648e81c
A01254093ASML <112582790+A01254093ASML@users.noreply.github.com>
2022-09-02 16:30:12 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(Número1 + 1)
+    Número1 += 1
+    basic.showNumber(Número1)
 })
 input.onButtonPressed(Button.AB, function () {
-    Texto1 += 1
+    Texto1 = "Sofia"
     basic.showString(Texto1)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(Número1 - 1)
+    Número1 += -1
+    basic.showNumber(Número1)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Texto1 = "Ana"
     basic.showString(Texto1)
 })
 let Número1 = 0
 let Texto1 = ""
-Texto1 = "Sofia"
+Texto1 = "Ana"
 Número1 = 5
